3f25905dc8501fa61cdd4e491574859603d608dd
Revanthshalon <revanthshalon@gmail.com>
2023-11-27 04:58:35 +0530

    AuthStack

---
+++ src/routes/AuthStack.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+import LoginScreen from "../ui/LoginScreen";
+import RegisterScreen from "../ui/RegisterScreen";
+
+export type AuthStackParamsList = {};
+
+const AuthStack = () => {
+  const Stack = createNativeStackNavigator();
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{ headerShown: false }}
+        initialRouteName="Login"
+      >
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Register" component={RegisterScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default AuthStack;
+
+const styles = StyleSheet.create({});
