2ac7ec2a17546912aaedbf4335ccbc0f1f7891e3
Revanthshalon <revanthshalon@gmail.com>
2023-11-27 12:33:26 +0530

    Root Stack

---
+++ src/routes/RootStack.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+type Props = {};
+
+const RootStack = (props: Props) => {
+  return (
+    <View>
+      <Text>RootStack</Text>
+    </View>
+  );
+};
+
+export default RootStack;
+
+const styles = StyleSheet.create({});
