283a0cc9634f71a38d3d9990c1d27fb9b7db38a3
Revanthshalon <revanthshalon@gmail.com>
2023-11-29 12:24:07 +0530

    Adding Seed Data for Company List

---
+++ src/data/seed.ts
@@ -0,0 +1,83 @@
+import Company from "../model/Company";
+
+const companyList: Company[] = [
+  {
+    _id: "1",
+    _uid: "1",
+    name: "Company 1",
+    balance: 100,
+    contact: "1234567890",
+  },
+  {
+    _id: "2",
+    _uid: "1",
+    name: "Company 2",
+    balance: 200,
+    contact: "1234567890",
+  },
+  {
+    _id: "3",
+    _uid: "1",
+    name: "Company 3",
+    balance: 300,
+    contact: "1234567890",
+  },
+  {
+    _id: "4",
+    _uid: "1",
+    name: "Company 4",
+    balance: 400,
+    contact: "1234567890",
+  },
+  {
+    _id: "5",
+    _uid: "1",
+    name: "Company 5",
+    balance: 500,
+    contact: "1234567890",
+  },
+  {
+    _id: "6",
+    _uid: "1",
+    name: "Company 6",
+    balance: 600,
+    contact: "1234567890",
+  },
+  {
+    _id: "7",
+    _uid: "1",
+    name: "Company 7",
+    balance: 700,
+    contact: "1234567890",
+  },
+  {
+    _id: "8",
+    _uid: "1",
+    name: "Company 8",
+    balance: 800,
+    contact: "1234567890",
+  },
+  {
+    _id: "9",
+    _uid: "1",
+    name: "Company 9",
+    balance: 900,
+    contact: "1234567890",
+  },
+  {
+    _id: "10",
+    _uid: "1",
+    name: "Company 10",
+    balance: 1000,
+    contact: "1234567890",
+  },
+  {
+    _id: "11",
+    _uid: "1",
+    name: "Company 11",
+    balance: 1100,
+    contact: "1234567890",
+  },
+];
+
+export default companyList;
