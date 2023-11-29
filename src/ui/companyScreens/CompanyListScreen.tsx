6ed7c8d90ab40dd9ddeee70a941e5cdcdb29fbb5
Revanthshalon <revanthshalon@gmail.com>
2023-11-29 12:24:22 +0530

    Setting Up Skeleton for Company List View

--- src/ui/companyScreens/CompanyListScreen.tsx
+++ src/ui/companyScreens/CompanyListScreen.tsx
@@ -1,19 +1,44 @@
-import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import companyList from "../../data/seed";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-type Props = {};
+const CompanyListScreen = () => {
+  const [companies, setCompanies] = React.useState(companyList);
 
-const CompanyListScreen = (props: Props) => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.label}>CompanyListScreen</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#212529" }}>
+      <View style={styles.container}>
+        <View>
+          {/* TODO: Add A Search Bar Component Here */}
+          <Text>Search Bar</Text>
+        </View>
+        <View style={styles.listContainer}>
+          {/* TODO: Add a List Rendering Component Here */}
+          {!companyList && (
+            <Text style={styles.label}>No Companies Added Yet</Text>
+          )}
+          {companyList && (
+            <FlatList
+              data={companyList}
+              keyExtractor={(item) => item._id}
+              renderItem={({ item }) => <Text>{item.name}</Text>}
+            />
+          )}
+        </View>
+        {/* Add a Floating Action Button Component Here */}
+      </View>
+    </SafeAreaView>
   );
 };
 
 export default CompanyListScreen;
 
 const styles = StyleSheet.create({
+  containerWrapper: {
+    flex: 1,
+    backgroundColor: "#212529",
+  },
   container: {
     flex: 1,
     backgroundColor: "#212529",
@@ -22,5 +47,9 @@ const styles = StyleSheet.create({
   },
   label: {
     color: "#f8f9fa",
+  },
+  listContainer: {
+    flex: 1,
+    backgroundColor: "#212529",
   },
 });
