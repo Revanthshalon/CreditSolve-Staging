b964e0367a7e054fde1afa654410014f585d4537
Revanthshalon <revanthshalon@gmail.com>
2023-11-29 06:16:38 +0530

    Defining Custom Drawer Component

---
+++ src/components/Drawer.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+type Props = {};
+
+const Drawer = (props: Props) => {
+  return (
+    <View>
+      <Text>Drawer</Text>
+    </View>
+  );
+};
+
+export default Drawer;
+
+const styles = StyleSheet.create({});
