d6be40c9dff97d91e1917c5c07df61cddafcecc0
Revanthshalon <revanthshalon@gmail.com>
2023-11-30 10:11:05 +0530

    Company List Updates

--- src/ui/companyScreens/CompanyListScreen.tsx
+++ src/ui/companyScreens/CompanyListScreen.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import { FlatList, StyleSheet, Text, View } from "react-native";
 import companyList from "../../data/seed";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { CompanyItem, SearchBar } from "../../components";
+import { CompanyItem, SearchBar, FloatingButton } from "../../components";
+import CompanyForm from "./CompanyForm";
+import Icons from "@expo/vector-icons/Ionicons";
 
 const CompanyListScreen = () => {
   const [companies, setCompanies] = React.useState(companyList);
   const [searchText, setSearchText] = React.useState("");
+  const [visibility, setVisibility] = React.useState(false);
 
   const filteredCompaniesByName = companies.filter((company) =>
     company.name.toLowerCase().includes(searchText.toLowerCase())
@@ -33,7 +36,18 @@ const CompanyListScreen = () => {
             />
           )}
         </View>
-        {/* Add a Floating Action Button Component Here */}
+        <FloatingButton
+          onPress={() => {
+            setVisibility(true);
+          }}
+          Icon={<Icons name="add" size={24} color="#f8f9fa" />}
+          style={styles.floatingButton}
+        />
+        <CompanyForm
+          visibility={visibility}
+          setVisibility={setVisibility}
+          formtype="add"
+        />
       </View>
     </SafeAreaView>
   );
@@ -59,5 +73,16 @@ const styles = StyleSheet.create({
     backgroundColor: "#212529",
     maxHeight: 620,
     width: "92%",
+  },
+  floatingButton: {
+    position: "absolute",
+    bottom: 80,
+    right: 20,
+    backgroundColor: "#39b54a",
+    width: 50,
+    height: 50,
+    borderRadius: 50,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
